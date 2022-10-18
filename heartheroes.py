46123ed6097a509470f622c21a4a48fb2761417a
Franziska Charlotte Renee Dreier <fdreier@mail.uni-mannheim.de>
2022-10-18 20:17:31 +0200

    feat: normalization (MinMax)

--- heartheroes.py
+++ heartheroes.py
@@ -16,7 +16,6 @@ print('Statistic of numerical features:')
 print(heart_disease.describe())
 print()
 
-# TODO @FD implement normalization of numerical features
 # Normalization of numerical features
 scaler = preprocessing.MinMaxScaler()
 
@@ -25,12 +24,13 @@ numerical_features_preprocessed = heart_disease.copy()
 numerical_features_preprocessed[["BMI", "PhysicalHealth", "MentalHealth", "SleepTime"]] = \
   scaler.fit_transform(numerical_features_preprocessed[["BMI", "PhysicalHealth", "MentalHealth", "SleepTime"]])
 
+print('Statistic of normalized numerical features:')
 print(numerical_features_preprocessed.describe())
 
 
 # Print correlation matrix of numerical features
 print('\nCorrelation matrix of numerical features:')
-print(heart_disease.corr())
+print(numerical_features_preprocessed.corr(numeric_only=True))
 
 # List all numerical features
 numerical_features = ["BMI", "PhysicalHealth", "MentalHealth", "SleepTime"]
@@ -42,12 +42,12 @@ figure_distribution.canvas.set_window_title('Distribution of numerical features'
 figure_index = 1
 for current_feature in numerical_features:
     ax = figure_distribution.add_subplot(2, 4, figure_index)
-    heart_disease.boxplot(column=[current_feature], by='HeartDisease', ax=ax)
+    numerical_features_preprocessed.boxplot(column=[current_feature], by='HeartDisease', ax=ax)
     figure_index += 1
 # Histogram
 for current_feature in numerical_features:
     ax = figure_distribution.add_subplot(2, 4, figure_index)
-    for has_heart_disease, group in heart_disease.groupby('HeartDisease'):
+    for has_heart_disease, group in numerical_features_preprocessed.groupby('HeartDisease'):
         # plot the data points for the current group and feature combination
         ax.hist(group[current_feature], label=has_heart_disease)
         ax.legend()
@@ -63,12 +63,11 @@ for current_x_feature in numerical_features:
     for current_y_feature in numerical_features:
         if current_x_feature != current_y_feature:
             ax = figure_scatter.add_subplot(4, 3, figure_index)
-            ax.scatter(heart_disease[current_x_feature], heart_disease[current_y_feature], color='black')
+            ax.scatter(numerical_features_preprocessed[current_x_feature],
+                       numerical_features_preprocessed[current_y_feature], color='black')
             plt.xlabel(current_x_feature)
             plt.ylabel(current_y_feature)
             figure_index += 1
 plt.tight_layout()
 
 plt.show()
-
-
